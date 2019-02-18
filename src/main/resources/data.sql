6e1c89631e384c011673b27473c1f36b4d001701
Brandon South <36432222+branthragan@users.noreply.github.com>
2019-02-18 11:43:44 -0600

    Setup h2 in-memory database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO inventory (id, name, total)
+VALUES (1001, 'beverage1', 5);
+INSERT INTO inventory (id, name, total)
+VALUES (1002, 'beverage2', 5);
+INSERT INTO inventory (id, name, total)
+VALUES (1003, 'beverage3', 5);
+INSERT INTO inventory (id, name, total)
+VALUES (1004, 'beverage4', 5);
+INSERT INTO inventory (id, name, total)
+VALUES (1005, 'beverage5', 5);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE inventory
+(
+  id    LONG NOT NULL,
+  name  CHAR(30),
+  total INTEGER,
+  PRIMARY KEY (id)
+);
